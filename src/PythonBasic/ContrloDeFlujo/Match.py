b04b27b0409aeca4396a2489dc9957fd9d5e1ede
Rivira Adrian <124736429+DonFierroFsa@users.noreply.github.com>
2023-05-09 11:42:02 -0300

    .

---
+++ src/PythonBasic/ContrloDeFlujo/Match.py
@@ -0,0 +1,14 @@
+valor="asdasd"
+#Switch
+match valor:
+    case 1:
+        print("valor es 1")
+
+    case 2:
+        print("Valor es 2")
+
+    case 3:
+        print("Valor es 3")
+
+    case _:
+        print("el valor es ",valor)
