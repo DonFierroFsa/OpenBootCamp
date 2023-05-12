10a87014eea1167c039fe52d348dd0ecb21318ff
Rivira Adrian <124736429+DonFierroFsa@users.noreply.github.com>
2023-05-12 09:28:38 -0300

    .

--- src/PythonBasic/Input _Output/PruebaBorrar.py
+++ src/PythonBasic/Input _Output/PruebaBorrar.py
@@ -1,8 +1,8 @@
 import pickle
-
+from SerializarObj import Vehiculo
 
 #Abro el contenido serializado de miAuto
-o = open("MiAuto.bin","rb")#Importante que sean datos binarios RB
-auto=pickle.load(o)
+o = open("MiAuto.bin", "rb")  # Importante que sean datos binarios RB
+auto = pickle.load(o)
 o.close()
-print(auto.arranca(),auto.modelo,auto.marca,auto.potencia)
+print(auto)
--- src/PythonBasic/Input _Output/SerializarObj.py
+++ src/PythonBasic/Input _Output/SerializarObj.py
@@ -1,20 +1,32 @@
 import pickle
 
 
-class Auto:
+class Vehiculo:
     modelo = ""
     marca = ""
     potencia = 0
     encendido = False
+
     def __init__(self, modelo, marca, potencia):
         self.marca = marca
         self.modelo = modelo
         self.potencia = potencia
 
+    def __str__(self):
+        return f"la marca es {self.marca}, el modelo es {self.modelo}, y la potencia es {self.potencia}"
+
     def arranca(self):
         print("el PODEROS FIAT UNO arranco su BESTIA_MOTOR")
         self.encendido = True
 
-miAuto = Auto("Uno", "Fiat", 320)
-print(miAuto.modelo, miAuto.marca, miAuto.potencia)
+
+miAuto = Vehiculo("Uno", "Fiat", 320)
+print(miAuto)
 miAuto.arranca()
+f = open("MiAuto.bin", "wb")
+pickle.dump(miAuto,f)
+f.close()
+o = open("MiAuto.bin", "rb")  # Importante que sean datos binarios RB
+auto = pickle.load(o)
+o.close()
+print(auto)
