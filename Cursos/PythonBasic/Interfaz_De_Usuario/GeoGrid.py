00918b68fec7a65eb0e2acec4380eca3c5a9f43d
Rivira Adrian <124736429+DonFierroFsa@users.noreply.github.com>
2023-05-15 18:22:13 -0300

    .

--- Cursos/PythonBasic/Interfaz_De_Usuario/GeoGrid.py
+++ Cursos/PythonBasic/Interfaz_De_Usuario/GeoGrid.py
@@ -12,18 +12,18 @@ username_label = ttk.Label(window, text="USER NAME:")
 username_label.grid(column=0, row=0,  # Elijo en que coordenanda esta
                     sticky=tkinter.W,  # Elijo su ubicacion como si fuera una brujula: "East","West","North",etc
                     padx=5, pady=5)  # Elijo el tamaño
-#Username Entry
+# Username Entry
 username_entry = ttk.Entry(window)
 username_entry.grid(column=1, row=0, sticky=tkinter.W, padx=5, pady=5)
 ##Password
-#Password Label
+# Password Label
 password = ttk.Label(window, text="PASSWORD")
 password.grid(column=0, row=1, sticky=tkinter.W, pady=7, padx=7)
-#Password Entry
-password_entry = ttk.Entry(window,show = "*")
-password_entry.grid(column=1,row=1,sticky=tkinter.W,padx=7,pady=7)
+# Password Entry
+password_entry = ttk.Entry(window, show="*")
+password_entry.grid(column=1, row=1, sticky=tkinter.W, padx=7, pady=7)
 ##LogginButon
-loggin_button = ttk.Button(window,text="Loggin")
-loggin_button.grid(column=1,row=3,sticky= tkinter.N,padx=10,pady=10)
+loggin_button = ttk.Button(window, text="Loggin")
+loggin_button.grid(column=1, row=3, sticky=tkinter.N, padx=10, pady=10)
 
 window.mainloop()
