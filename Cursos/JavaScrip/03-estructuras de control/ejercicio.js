69f3ef9488e099206c509ba6f86e0452e8127ab8
Adrian <riviraadrian@hotmail.com>
2023-06-05 17:35:41 -0300

    .

---
+++ Cursos/JavaScrip/03-estructuras de control/ejercicio.js
@@ -0,0 +1,24 @@
+//Ejercicio
+//factorial con bucle for
+var numero=4;
+var factorial1=1;
+
+for (let i=1; i<=numero; i++){
+
+    factorial1=factorial1*i
+    console.log(factorial1);
+}
+console.log(factorial1,"calculado con for")
+
+//facorial While
+
+var factorial2=1;
+var j=1;
+while (j<=numero){
+    factorial2=factorial2*j
+    j++;
+}
+console.log(factorial2,"calulado con while")
+
+//factorial While break and IF
+
