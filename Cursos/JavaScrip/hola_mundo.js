7cbd157f91018709e2503769b8d8475d9352fa34
Adrian <riviraadrian@hotmail.com>
2023-06-05 09:28:17 -0300

    .

---
+++ Cursos/JavaScrip/hola_mundo.js
@@ -0,0 +1,10 @@
+console.log("Hola Mundo");
+
+//En la carpeta escribo el comando "npm init"--> luego el nombre del proyecto asi  como las especificaciones que arriba se detallan
+// node hola_mundo.js
+//npm run test--> hace referencia a los "scrips"
+//npm run test  ----- npm run saludar
+/*"scrip":{
+    "start":"node index.js",
+    "saludar" : "node hola_mundo.js"
+}*/
